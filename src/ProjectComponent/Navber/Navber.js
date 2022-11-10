07a1f87a130941358dd8ff2154f67c3a6ffc3901
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2022-11-10 23:11:19 +0600

    Tiltle add  foter addid

--- src/ProjectComponent/Navber/Navber.js
+++ src/ProjectComponent/Navber/Navber.js
@@ -19,7 +19,7 @@ const Navber = () => {
   }
     return (
         <div className="navbar bg-base-100">
-        <div className="navbar-start">
+        {/* <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
@@ -29,7 +29,8 @@ const Navber = () => {
            
             </ul>
           </div>
-        </div>
+        </div> */}
+        <h3 className='nav-start text-3xl text-red-300'>Mothers Delivery Foods</h3>
         <div className="navbar">
          
            <Link to='/'  className="btn btn-ghost normal-case text-xl">HomePage</Link>
